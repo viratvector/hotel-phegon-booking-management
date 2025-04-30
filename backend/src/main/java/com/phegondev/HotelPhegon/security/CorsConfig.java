f58a98b0d0c95889cd82f1e16d86d814a1b141e2
Virat Srivastava <virat.srivastava@simpplr.com>
2025-04-30 20:14:42 +0530

    cors change

--- backend/src/main/java/com/phegondev/HotelPhegon/security/CorsConfig.java
+++ backend/src/main/java/com/phegondev/HotelPhegon/security/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*");
+                        ..allowedOrigins("https://phegon-hotel-group.netlify.app")
+                        .allowCredentials(true);
             }
         };
     }
